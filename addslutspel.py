e0787e51a6288d7458b2456d1b7a2f675c4e7d13
claraxc <102219687+claraxc@users.noreply.github.com>
2022-03-23 10:30:41 +0100

    Add files via upload

---
+++ addslutspel.py
@@ -0,0 +1,76 @@
+from random import randint
+
+def slutspel(savedsum, dice4, dice5):
+    r = 10000000                 # antalet gånger simulationen ska köras
+    savedsum += dice4       # de sparade tärningarnas summa med 4e tärningen
+
+    wins = 0
+    losses = 0
+
+    for i in range(r):
+        dice6 = randint(1, 6)                   # slår 6e tärningen
+        #print("hitting dice6:", dice6)
+        #print("savedsum:", savedsum + dice5 + dice6)
+        if savedsum + dice5 + dice6 >= 30:      # om man når 30
+            wins += 1
+            #print("wins:", wins, "\n")
+        elif savedsum + dice5 + dice6 < 30:     # om man inte når 30
+            losses += 1
+            #print("losses:", losses, "\n")
+
+    savewinrate = wins / r * 100            # sannolikheten att vinna om man sparar 5e tärningen
+    savelossrate = losses / r * 100         # sannolikheten att förlora
+
+    # om man slår om 5:e tärningen
+    #print("hit\n")
+    wins = 0
+    losses = 0
+
+    for i in range(r):
+        dice5 = randint(1, 6)                   # slår om 5e och 6e tärningen
+        dice6 = randint(1, 6)
+        #print("try 1:")
+        #print("hitting dice5:", dice5)
+        #print("hitting dice6:", dice6)
+        #print("savedsum:", savedsum + dice5 + dice6)
+        if savedsum + dice5 + dice6 >= 30:      # om man når 30
+            wins += 1
+            #print("wins:", wins, "\n")
+        elif savedsum + dice5 + dice6 < 30:     # om man inte når 30
+            #print("2.")
+            if dice5 < dice6:
+                dice5 = randint(1, 6)
+                #print("saving one dice:", dice6)
+                #print("hitting the other:", dice5)
+            else:
+                dice6 = randint(1, 6)           # spara en tärning, slå om den andra
+                #print("saving one dice:", dice5)
+                #print("hitting the other:", dice6)
+            #print("savedsum", savedsum + dice5 + dice6)
+            if savedsum + dice5 + dice6 >= 30:
+                wins += 1
+                #print("wins:", wins, "\n")
+            elif savedsum + dice5 + dice6 < 30:
+                losses += 1
+                #print("losses:", losses, "\n")
+
+    hitwinrate = wins / r * 100             # sannolikheten att vinna om man slår om 5e och 6e tärningen
+    hitlossrate = losses / r * 100          # sannolikheten att förlora
+
+    # statistik för om man sparar 5e tärningen
+    #print("\nSavewinrate: {:.2f}%".format(savewinrate))
+    #print("Savelossrate: {:.2f}%".format(savelossrate))
+
+    # statistik för om man slår om 5e och 6e tärningen
+    #print("\nHitwinrate: {:.2f}%".format(hitwinrate))
+    #print("Hitlossrate: {:.2f}%\n".format(hitlossrate))
+
+    if savewinrate > hitwinrate:        # om man har störst chans att vinna genom att spara
+        #print("Action: Save\n")
+        return "save"
+    elif savewinrate < hitwinrate:      # om man har störst chans att vinna genom att slå om
+        #print("Action: Hit\n")
+        return "hit"
+    else:                               # om sannolikheten är lika stor
+        #print("Action: -\n")
+        return "save"
---
+++ slutspel.py
@@ -0,0 +1,76 @@
+from random import randint
+
+r = 1000000             # antalet gånger simulationen ska köras
+savedsum = 20           # de sparade tärningarnas summa med 4e tärningen
+dice5 = 5               # den 5:e tärningens värde som man kan välja att spara eller ej
+
+print(r, savedsum, dice5)
+
+wins = 0
+losses = 0
+
+for i in range(r):
+    dice6 = randint(1, 6)                   # slår 6e tärningen
+    if savedsum + dice5 + dice6 >= 30:      # om man når 30
+        wins += 1
+    elif savedsum + dice5 + dice6 < 30:     # om man inte når 30
+        losses += 1
+
+savewinrate = wins / r * 100            # sannolikheten att vinna om man sparar 5e tärningen
+savelossrate = losses / r * 100         # sannolikheten att förlora
+
+wins = 0
+losses = 0
+
+for i in range(r):
+    dice5 = randint(1, 6)                   # slår om 5e och 6e tärningen
+    dice6 = randint(1, 6)
+    if savedsum + dice5 + dice6 >= 30:      # om man når 30 / instant win
+        wins += 1
+    elif savedsum + dice5 + dice6 < 30:     # om man inte når 30
+        if dice5 < dice6:
+            dice5 = randint(1, 6)
+        else:
+            dice6 = randint(1, 6)               # spara en tärning, slå om den andra
+        if savedsum + dice5 + dice6 >= 30:
+            wins += 1
+        elif savedsum + dice5 + dice6 < 30:
+            losses += 1
+
+hitwinrate = wins / r * 100             # sannolikheten att vinna om man slår om 5e och 6e tärningen
+hitlossrate = losses / r * 100          # sannolikheten att förlora
+
+# statistik för om man sparar 5e tärningen
+print("\nSavewinrate: {}%".format(round(savewinrate, 2)))
+print("Savelossrate: {}%".format(round(savelossrate, 2)))
+
+# statistik för om man slår om 5e och 6e tärningen
+print("\nHitwinrate: {}%".format(round(hitwinrate, 2)))
+print("Hitlossrate: {}%\n".format(round(hitlossrate, 2)))
+
+if savewinrate > hitwinrate:        # om man har störst chans att vinna genom att spara
+    print("Action: Save")
+elif savewinrate < hitwinrate:      # om man har störst chans att vinna genom att slå om
+    print("Action: Hit")
+else:                               # om sannolikheten är lika stor
+    print("Action: -")
+
+"""100000 21 3
+
+Savewinrate: 16.84%
+Savelossrate: 83.17%
+
+Hitwinrate: 27.82%
+Hitlossrate: 72.19%
+
+Actiom: Hit"""
+
+"""100000 21 3
+
+Savewinrate: 16.68%
+Savelossrate: 83.33%
+
+Hitwinrate: 41.67%
+Hitlossrate: 58.33%
+
+Actiom: Hit"""
